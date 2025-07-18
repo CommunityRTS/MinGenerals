36c621c9d3ace47fc3c582bba04c52892f00956e
aliendroid1 <9aliquraishi9@gmail.com>
2025-07-18 12:37:03 -0500

    docs: Update in-source legal notices
    In preparation for merging the Generals and GeneralsMD projects,
    all relevant source files have been updated to include both
    "Command & Conquer Generals(tm)" and
    "Command & Conquer Generals Zero Hour(tm)" trademark and
    copyright notices. This ensures the combined project accurately
    documents its origins and complies with Electronic Arts’ legal
    requirements.
    
    CommunityRTS (c) and MinGenerals(tm) have been formally
    introduced as the project’s organizational and codebase identities.
    A CommunityRTS copyright notice has been added so that future
    changes can be made in accordance with license requirements and
     to clarify the authorship of ongoing modifications.

--- Generals/Code/Libraries/Source/Compression/Compression.h
+++ Generals/Code/Libraries/Source/Compression/Compression.h
@@ -1,19 +1,29 @@
 /*
-**	Command & Conquer Generals(tm)
-**	Copyright 2025 Electronic Arts Inc.
+** MinGenerals(tm)
+** Copyright 2025 CommunityRTS
 **
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
+** The above copyright notice applies to additions and/or other modifications
+** made to this file by CommunityRTS.
 **
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+** This file incorporates original work covered by the following copyright and
+** permission notice:
 **
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**		Command & Conquer Generals(tm)
+**		Command & Conquer Generals Zero Hour(tm)
+**		Copyright 2025 Electronic Arts Inc.
+**
+**		This program is free software: you can redistribute it and/or modify
+**		it under the terms of the GNU General Public License as published by
+**		the Free Software Foundation, either version 3 of the License, or
+**		(at your option) any later version.
+**
+**		This program is distributed in the hope that it will be useful,
+**		but WITHOUT ANY WARRANTY; without even the implied warranty of
+**		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**		GNU General Public License for more details.
+**
+**		You should have received a copy of the GNU General Public License
+**		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // FILE: Compression.h ///////////////////////////////////////////////////////
--- GeneralsMD/Code/Tools/WorldBuilder/include/TeamBehavior.h
+++ GeneralsMD/Code/Tools/WorldBuilder/include/TeamBehavior.h
@@ -1,19 +1,29 @@
 /*
-**	Command & Conquer Generals Zero Hour(tm)
-**	Copyright 2025 Electronic Arts Inc.
+** MinGenerals(tm)
+** Copyright 2025 CommunityRTS
 **
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
+** The above copyright notice applies to additions and/or other modifications
+** made to this file by CommunityRTS.
 **
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+** This file incorporates original work covered by the following copyright and
+** permission notice:
 **
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**		Command & Conquer Generals(tm)
+**		Command & Conquer Generals Zero Hour(tm)
+**		Copyright 2025 Electronic Arts Inc.
+**
+**		This program is free software: you can redistribute it and/or modify
+**		it under the terms of the GNU General Public License as published by
+**		the Free Software Foundation, either version 3 of the License, or
+**		(at your option) any later version.
+**
+**		This program is distributed in the hope that it will be useful,
+**		but WITHOUT ANY WARRANTY; without even the implied warranty of
+**		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**		GNU General Public License for more details.
+**
+**		You should have received a copy of the GNU General Public License
+**		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #if !defined(AFX_TEAMBEHAVIOR_H__40A707E0_08CB_4544_9FF1_AF0DACAB02A6__INCLUDED_)
